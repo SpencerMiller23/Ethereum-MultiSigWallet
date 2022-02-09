2af8ba51780cacf77fd5c1adee9426be61f9a38f
Spencer Miller <spencer.miller@me.com>
2022-02-09 22:22:53 +0200

    Added link to homepage in menu bar

--- components/MenuBar/MenuBar.js
+++ components/MenuBar/MenuBar.js
@@ -6,6 +6,8 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 
+import Link from 'next/link'
+
 import styles from './MenuBar.module.css';
 
 const MenuBar = () => {
@@ -14,7 +16,9 @@ const MenuBar = () => {
             <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static" position="absolute">
                     <Toolbar className={styles.toolbar}>
-                        <h3>Multi-Sig Wallet</h3>
+                        <Link href="/">
+                            <h3>Multi-Sig Wallet</h3>
+                        </Link>
                         <ConnectWallet/>
                     </Toolbar>
                 </AppBar>
