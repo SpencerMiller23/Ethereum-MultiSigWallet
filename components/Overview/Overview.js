2380b8e7f9c72c343349705eb107429ee35a6f67
Spencer Miller <spencer.miller@me.com>
2022-02-13 22:00:56 +0200

    Created Overview component

--- components/Overview/Overview.js
+++ components/Overview/Overview.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import { Paper, Button } from '@mui/material'
+import { Paper } from '@mui/material'
 
-const TransactionQueue = () => {
+const Overview = () => {
   return (
     <div>
         <Paper sx={{ backgroundColor: 'light.main', p: '20px', mt: '20px' }}>
-            <h4>Pending Transactions</h4>
+            <h4>Overview</h4>
         </Paper>
     </div>
   )
 }
 
-export default TransactionQueue
+export default Overview
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import { MenuBar } from "../components/MenuBar";
 import { Drawer } from "../components/Drawer";
 import { CreateWallet } from "../components/CreateWallet";
+import { Overview } from "../components/Overview";
+
 import { Grid } from "@mui/material";
 
 export default function Home() {
@@ -10,7 +12,9 @@ export default function Home() {
         <Grid item xs={12} md={8}>
           <CreateWallet/>
         </Grid>
-        <Grid item xs={12} md={4}></Grid>
+        <Grid item xs={12} md={4}>
+          <Overview/>
+        </Grid>
       </Grid>
     </div>
   )
