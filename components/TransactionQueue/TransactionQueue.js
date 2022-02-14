9a9a8300506c6afa94a20e7cf1a336357f3ac088
Spencer Miller <spencer.miller@me.com>
2022-02-14 22:06:17 +0200

    Created fetchTransactions function

--- components/TransactionQueue/TransactionQueue.js
+++ components/TransactionQueue/TransactionQueue.js
@@ -1,33 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import { Paper, Button, Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@mui/material'
+import { ethers } from 'ethers'
+
+import { Paper, Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@mui/material'
+
+import MultiSigWallet from '../../artifacts/contracts/MultiSigWallet.sol/MultiSigWallet.json'
 
 const TransactionQueue = ({ address }) => {
-  return (
-    <div>
-        <Paper sx={{ backgroundColor: 'light.main', mt: '20px' }}>
-            <h4>Pending Transactions</h4>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Recipient</TableCell>
-                            <TableCell>Value</TableCell>
-                            <TableCell></TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell>0x</TableCell>
-                            <TableCell>1.0 ETH</TableCell>
-                            <TableCell>Approve</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Paper>
-    </div>
-  )
+
+    useEffect(async () => {
+        if (address) {
+            await fetchTransactions()
+        }
+    }, [address])
+
+    const fetchTransactions = async () => {
+        try {
+            const provider = new ethers.providers.Web3Provider(window.web3.currentProvider)
+            const wallet = new ethers.Contract(address, MultiSigWallet.abi, provider)
+            const transactions = await wallet.transactions(0)
+            console.log(transactions)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <div>
+            <Paper sx={{ backgroundColor: 'light.main', mt: '20px' }}>
+                <h4>Pending Transactions</h4>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Recipient</TableCell>
+                                <TableCell>Value</TableCell>
+                                <TableCell>Calldata</TableCell>
+                                <TableCell>Action</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>0x</TableCell>
+                                <TableCell>1.0 ETH</TableCell>
+                                <TableCell>0x</TableCell>
+                                <TableCell>Approve</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </div>
+    )
 }
 
 export default TransactionQueue
