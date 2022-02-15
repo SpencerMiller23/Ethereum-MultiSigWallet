7746eaae69d30cf8d9accf5735394cac93befa0b
Spencer Miller <spencer.miller@me.com>
2022-02-15 22:38:50 +0200

    Changed executed boolean to status enum

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -8,11 +8,17 @@ contract MultiSigWallet {
     event Revoke(address indexed owner, uint indexed txId);
     event Execute(uint indexed txId);
 
+    enum Status {
+        Pending,
+        Executed,
+        Rejected
+    }
+
     struct Transaction {
         address to;
         uint value;
         bytes data;
-        bool executed;
+        Status status;
     }
 
     address[] public owners;
@@ -37,8 +43,8 @@ contract MultiSigWallet {
         _;
     }
 
-    modifier notExecuted(uint _txId) {
-        require(!transactions[_txId].executed, "tx already executed");
+    modifier isPending(uint _txId) {
+        require(transactions[_txId].status == Status.Pending, "tx already executed");
         _;
     }
 
@@ -67,12 +73,12 @@ contract MultiSigWallet {
             to: _to,
             value: _value,
             data: _data,
-            executed: false
+            status: Status.Pending
         }));
         emit Submit(transactions.length - 1);
     }
 
-    function approve(uint _txId) external onlyOwner txExists(_txId) notApproved(_txId) notExecuted(_txId) {
+    function approve(uint _txId) external onlyOwner txExists(_txId) notApproved(_txId) isPending(_txId) {
         approved[_txId][msg.sender] = true;
         emit Approve(msg.sender, _txId);
     }
@@ -85,10 +91,10 @@ contract MultiSigWallet {
         }
     }
 
-    function execute(uint _txId) external txExists(_txId) notExecuted(_txId) {
+    function execute(uint _txId) external txExists(_txId) isPending(_txId) {
         require(_getApprovalCount(_txId) >= required, "Not enough approvals");
         Transaction storage transaction = transactions[_txId];
-        transaction.executed = true;
+        transaction.status = Status.Executed;
         (bool success, ) = transaction.to.call{value: transaction.value}(
             transaction.data
         );
@@ -96,7 +102,7 @@ contract MultiSigWallet {
         emit Execute(_txId);
     }
 
-    function revoke(uint _txId) external onlyOwner txExists(_txId) notExecuted(_txId) {
+    function revoke(uint _txId) external onlyOwner txExists(_txId) isPending(_txId) {
         require(approved[_txId][msg.sender], "tx not approved");
         approved[_txId][msg.sender] = false;
         emit Revoke(msg.sender, _txId);
