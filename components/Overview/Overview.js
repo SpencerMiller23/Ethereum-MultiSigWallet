1db3535f84bda1f51fa58bc2fdaf916dfb8c211f
Spencer Miller <spencer.miller@me.com>
2022-02-21 19:36:00 +0200

    Added how it works section

--- components/Overview/Overview.js
+++ components/Overview/Overview.js
@@ -6,7 +6,10 @@ const Overview = () => {
   return (
     <div>
         <Paper sx={{ backgroundColor: 'light.main', p: '20px', mt: '20px' }}>
-            <h4>Overview</h4>
+            <h4>How it works</h4>
+            <p>A multi-signature wallet is a cryptocurrency wallet that requires multiple signatures from multiple owners to perform a transaction.</p>
+            <p>These types of wallets help to mitigate security concerns that come with a signle private key mechanism.</p>
+            <p>Create a new multi-signature wallet by giving it a memorable name, and filling in the addresses of any number of owners along with the number of required signatures.</p>
         </Paper>
     </div>
   )
